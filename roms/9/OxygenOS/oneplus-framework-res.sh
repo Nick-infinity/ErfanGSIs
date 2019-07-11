e3460dbf4f95e1c980e9531f0f5e8d03a46ad1fe
Nick-Infinity <nikhilg.me@gmail.com>
2019-07-11 15:31:11 +0000

    tissot: Added tissot specific overlays
    Signed-off-by: Nick-Infinity <nikhilg.me@gmail.com>

--- roms/9/OxygenOS/oneplus-framework-res.sh
+++ roms/9/OxygenOS/oneplus-framework-res.sh
@@ -5,3 +5,10 @@ apppath=$1
 
 find $apppath/res/ -type f -exec sed -i 's/@android/@*android/g' {} +
 sed -i 's/<bool name="config_has_rounded_corner">true/<bool name="config_has_rounded_corner">false/' $apppath/res/values/bools.xml
+echo "Adding tissot stuff"
+rm -rf $apppath/res/xml/power_profile.xml && cp -r $LOCALDIR/MiA1/xml/power_profile.xml $apppath/res/xml/power_profile.xml
+rm -rf $apppath/res/xml/power_profile.xml && cp -r $LOCALDIR/MiA1/xml/power_profile_test.xml $apppath/res/xml/power_profile_test.xml
+echo "Added power profiles for tissot"
+sed -i "6i <bool name="config_ignoreRssnrSignalLevel">true</bool>" $apppath/res/values/bools.xml
+sed -i "6i <integer name="config_deviceHardwareKeys">83</integer>" $apppath/res/values/integers.xml
+echo "Added configs for tissot"
